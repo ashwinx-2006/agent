4bd08addd1518f54c4b8a7a7fc6ebcd25ffb0d05
ashwinx-2006 <ssn.ashwin@gmail.com>
2026-09-08 14:33:05 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -55,3 +55,21 @@ for pattern in patterns:
     pattern,
     text
   )
+
+if match:
+  query = match.group(1)
+  break
+
+
+query = query.strip()
+
+video_id= get_vid(query)
+
+if not video_id:
+  return None
+
+return(
+  "https://www.youtube.com/embed/"
+  + video_id 
+  + "?autoplay=1&mute=0"
+)
